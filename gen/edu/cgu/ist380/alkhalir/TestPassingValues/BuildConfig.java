2521db0a818c8f046b536712cc6fd7a19e090cd0
Raeed Alkhalifa <raeed.khalifa@gmail.com>
2013-06-01 15:24:16 -0700

    First Commit

---
+++ gen/edu/cgu/ist380/alkhalir/TestPassingValues/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.cgu.ist380.alkhalir.TestPassingValues;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
